91bb36f6c9d7eb1e63ba021a764ea2f426fe4019
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2024-01-14 14:16:07 -0300

    feat(clients): add client entity

---
+++ src/main/java/com/transactiontgid/demo/models/entities/Client.java
@@ -0,0 +1,68 @@
+package com.transactiontgid.demo.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Clients")
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String naturalPersonRegistry;
+
+  @Column(nullable = false)
+  private String email;
+
+  public Client() {
+  }
+
+  public Client(Long id, String name, String naturalPersonRegistry, String email) {
+    this.id = id;
+    this.name = name;
+    this.naturalPersonRegistry = naturalPersonRegistry;
+    this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getNaturalPersonRegistry() {
+    return naturalPersonRegistry;
+  }
+
+  public void setNaturalPersonRegistry(String naturalPersonRegistry) {
+    this.naturalPersonRegistry = naturalPersonRegistry;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
