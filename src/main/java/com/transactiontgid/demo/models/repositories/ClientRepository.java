46e7b4e28a15d4c73bde2be689117b0e39959ee9
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2024-01-15 00:53:41 -0300

    feat(ClientRepository): findBy naturalRegistry added

--- src/main/java/com/transactiontgid/demo/models/repositories/ClientRepository.java
+++ src/main/java/com/transactiontgid/demo/models/repositories/ClientRepository.java
@@ -1,10 +1,11 @@
 package com.transactiontgid.demo.models.repositories;
 
 import com.transactiontgid.demo.models.entities.Client;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+  Optional<Client> findByNaturalPersonRegistry(String naturalPersonRegistry);
 }
