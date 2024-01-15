50401ed14be9b25e9a0a372a9764a4e8d3cc8a66
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2024-01-15 01:37:12 -0300

    feat(CompanyRepository): implements findByLegalPersonRegistry method

--- src/main/java/com/transactiontgid/demo/models/repositories/CompanyRepository.java
+++ src/main/java/com/transactiontgid/demo/models/repositories/CompanyRepository.java
@@ -1,10 +1,11 @@
 package com.transactiontgid.demo.models.repositories;
 
 import com.transactiontgid.demo.models.entities.Company;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-
+  Optional<Company> findByLegalPersonRegistry(String legalPersonRegistry);
 }
